d99268bc4a0451479bff33329504239006b0368b
Pratham Shroff <pshroff@bu.edu>
2023-05-10 21:58:37 -0400

    Switched back to Simplistic MVC for better UI

--- app/src/main/java/com/example/fieldtrack/RecordVisitActivity.kt
+++ app/src/main/java/com/example/fieldtrack/RecordVisitActivity.kt
@@ -5,74 +5,64 @@ import android.os.Bundle
 import android.widget.*
 
 
-class RecordVisitActivity : AppCompatActivity() {
+// TODO: Implement View Binding
+// TODO: Implement Form Validation
+// TODO: Implement Handling of Exceptions
 
-    // Declare fields at the class level so you can access them later
-    private lateinit var etBranchName: EditText
-    private lateinit var etUserName: EditText
-    private lateinit var etCheckIn: EditText
-    private lateinit var etCheckOut: EditText
-    private lateinit var etCompanyName: EditText
-    private lateinit var etContactPerson: EditText
-    private lateinit var rgPurpose: RadioGroup
-    private lateinit var etAmount: EditText
-    private lateinit var etNotes: EditText
-
+class RecordVisitActivity : AppCompatActivitys() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_visit)
 
-        val formLayout = findViewById<LinearLayout>(R.id.formLayout)
-
-        // Create the fields
-        etBranchName = createEditText("Branch Name")
-        etUserName = createEditText("User Name")
-        etCheckIn = createEditText("Check-in Time")
-        etCheckOut = createEditText("Check-out Time")
-        etCompanyName = createEditText("Company Name")
-        etContactPerson = createEditText("Contact Person Name")
-        rgPurpose =
-            createRadioGroup("Initial Purpose", arrayOf("Order-Lead", "Cold-Visit", "Collection"))
-        etAmount = createEditText("Amount")
-        etNotes = createEditText("Brief Meeting Notes")
-
-        // Add fields to the layout
-        formLayout.addView(etBranchName)
-        formLayout.addView(etUserName)
-        formLayout.addView(etCheckIn)
-        formLayout.addView(etCheckOut)
-        formLayout.addView(etCompanyName)
-        formLayout.addView(etContactPerson)
-        formLayout.addView(rgPurpose)
-        formLayout.addView(etAmount)
-        formLayout.addView(etNotes)
-
-        // Set fields as non-editable as required
-        etBranchName.isEnabled = false
-        etUserName.isEnabled = false
-        etCheckIn.isEnabled = false
-        etCheckOut.isEnabled = false
-
-        // Fetch and set data for non-editable fields
-        // (Replace "Placeholder" with actual data)
-        etBranchName.setText("Placeholder")
-        etUserName.setText("Placeholder")
-        etCheckIn.setText("Placeholder")
-        etCheckOut.setText("Placeholder")
-    }
-
-    private fun createEditText(hint: String): EditText {
-        return EditText(this).apply {
-            this.hint = hint
+        val submitButton = findViewById<Button>(R.id.submit_button)
+        submitButton.setOnClickListener {
+            // Handle form submission here
+            handleSubmitForm()
         }
     }
 
-    private fun createRadioGroup(title: String, options: Array<String>): RadioGroup {
-        val radioGroup = RadioGroup(this)
-        radioGroup.addView(TextView(this).apply { text = title })
-        for (option in options) {
-            radioGroup.addView(RadioButton(this).apply { text = option })
-        }
-        return radioGroup
+    private fun handleSubmitForm() {
+        // Fetch form data here
+        val branchName = findViewById<TextView>(R.id.branch_name).text.toString()
+        val userName = findViewById<TextView>(R.id.user_name).text.toString()
+        val checkinTime = findViewById<EditText>(R.id.checkin_time).text.toString()
+        val checkoutTime = findViewById<EditText>(R.id.checkout_time).text.toString()
+        val companyName = findViewById<EditText>(R.id.company_name).text.toString()
+        val contactPerson = findViewById<EditText>(R.id.contact_person).text.toString()
+
+        val initialPurposeGroup = findViewById<RadioGroup>(R.id.initial_purpose)
+        val selectedPurposeId = initialPurposeGroup.checkedRadioButtonId
+        val initialPurpose = findViewById<RadioButton>(selectedPurposeId)?.text.toString()
+
+        val amount = findViewById<EditText>(R.id.amount).text.toString()
+        val meetingNotes = findViewById<EditText>(R.id.meeting_notes).text.toString()
+
+        // Create a data object
+        val visitData = VisitData(
+            branchName,
+            userName,
+            checkinTime,
+            checkoutTime,
+            companyName,
+            contactPerson,
+            initialPurpose,
+            amount.toDoubleOrNull(),
+            meetingNotes
+        )
+
+        // TODO: Send visitData to your server or database
     }
 }
+
+// Data class to hold the form data
+data class VisitData(
+    val branchName: String,
+    val userName: String,
+    val checkinTime: String,
+    val checkoutTime: String,
+    val companyName: String,
+    val contactPerson: String,
+    val initialPurpose: String,
+    val amount: Double?,
+    val meetingNotes: String
+)
